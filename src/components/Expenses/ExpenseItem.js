fe79d0df6cba7c241d860c3bd5ff24cff37a82c4
ClaudiuHancu <claudiuhancu@gmail.com>
2021-06-15 10:04:58 +0300

    salvare

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,16 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  // const [title, setTitle] = useState(props.title);
+  // console.log(`cate staturi`);
+  // const clickHandler = () => {
+  //   setTitle(`Updated!`);
+  //   console.log(title);
+  // };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,7 +18,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
-}
+};
 export default ExpenseItem;
